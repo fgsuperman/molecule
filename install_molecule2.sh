a55aee04902ed4f4206a157ece6e1f011ce8cb4e
fgsuperman <43178948+fgsuperman@users.noreply.github.com>
2022-04-06 11:47:41 +0200

    Update install_molecule2.sh

--- install_molecule2.sh
+++ install_molecule2.sh
@@ -14,7 +14,7 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmo
 sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 echo "--> add user to docker group"
